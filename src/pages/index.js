8fb1553c4921a032f5f9b54c38d2ba1e1950417f
alphak <akajoseph344@gmail.com>
2024-12-13 09:44:30 +0000

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,38 +25,6 @@ export default function Home() {
       className={
         "h-screen w-full bg-white flex flex-col items-center justify-center"
       }
-    >
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex w-8/12 mx-auto items-center space-x-2"
-      >
-        <Input
-          type="query"
-          maxLength={70}
-          placeholder="What is your post about? (100 characters .max)"
-          {...register("query")}
-        />
-        <Button type="submit" disabled={loading}>
-          {loading && <Loader2 className="animate-spin" />} Generate a
-          description
-        </Button>
-      </form>
-
-      {description && !loading && (
-        <div className="mt-3 flex w-8/12 mx-auto items-center space-x-2 py-2 px-3 rounded-md bg-slate-300">
-          <p className="text-sm">{description}</p>
-        </div>
-      )}
-
-      {!loadingImage ? (
-        <div className="h-[300px] w-[300px] mx-auto mt-3 bg-slate-300 overflow-hidden rounded-sm">
-          {url && <img src={url} className="h-full w-full" />}
-        </div>
-      ) : (
-        <p className="mt-5 text-sm mx-auto self-center">
-          loading image, please wait...
-        </p>
-      )}
-    </div>
+    ></div>
   );
 }
